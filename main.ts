c0a3ecc5ff4a98950462da3fc147c7263076c8f0
zx6r000 <140099462+zx6r000@users.noreply.github.com>
2023-08-12 09:07:54 +0200

    new test

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
-    basic.showString("Hello!")
+	
 })
 makerbit.onIrDatagram(function () {
     basic.showString(makerbit.irDatagram())
 })
-makerbit.connectIrReceiver(DigitalPin.P0, IrProtocol.NEC)
+makerbit.connectIrReceiver(DigitalPin.P2, IrProtocol.NEC)
 basic.forever(function () {
 	
 })
