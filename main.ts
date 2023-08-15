f60a7f7e254f31075aac5aab1b7a46509dec6800
zx6r000 <140099462+zx6r000@users.noreply.github.com>
2023-08-15 23:35:23 +0200

    test avec diode emettrice

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    makerbit.sendIrDatagram("0x00FF02FD")
+})
 makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
 	
 })
@@ -5,6 +8,7 @@ makerbit.onIrDatagram(function () {
     basic.showString(makerbit.irDatagram())
 })
 makerbit.connectIrReceiver(DigitalPin.P2, IrProtocol.NEC)
+makerbit.connectIrSenderLed(AnalogPin.P1)
 basic.forever(function () {
 	
 })
